f709968b0969d4a240b612e1b228914b15c3c87e
Mangal Hang Limbu <lazehang@gmail.com>
2022-09-01 11:01:56 +0800

    update

--- gatsby-browser.js
+++
@@ -1,8 +0,0 @@
-// normalize CSS across browsers
-import './src/normalize.css'
-import './src/assets/scss/app.scss'
-// custom CSS styles
-import './src/style.css'
-
-// Highlighting for code blocks
-import 'prismjs/themes/prism.css'
